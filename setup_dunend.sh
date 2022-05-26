9da592b3e907e15a1940580b9886ad6f626e8e84
Tingjun Yang <tjyang@fnal.gov>
2022-05-26 10:37:19 -0500

    Add a setup script.

---
+++ setup_dunend.sh
@@ -0,0 +1,4 @@
+source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+setup larsoft v09_52_01 -q e20:prof
+setup genie_phyopt v3_00_04 -q dkcharmtau
+setup genie_xsec v3_00_04a -q G1810a0211a:e1000:k250
